9298debba37d1b2e58ee72432f9c62b2572fd95e
hakanhammarin <hakan.hammarin@gmail.com>
2014-04-19 07:35:34 +0200

    10-mila webresultat

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+	$config['server'] = "http://192.168.1.23:8080";
+	$config['refreshtime'] = 6;
+	$config['timezone'] = "Europe/Stockholm";
+	$config['timing'] = false;
+	$config['servertrackstatus'] = true;
+	$config['cache'] = false;
+	$config['cachetime'] = 60;
+	$config['cachefolder'] = "cache\\";
+	$config['cachetrackstatus'] = false;
+	$config['cachetrackstatustime'] = 60;
+?>
